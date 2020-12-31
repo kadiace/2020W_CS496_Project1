c5c766eef761bc11fdbc9bad40fd958b6cf53208
kadiace <rlacjfghks95@gmail.com>
2020-12-31 16:24:14 +0900

    scroll bar complete

--- app/src/main/java/com/example/project1/PhoneBookData.kt
+++ app/src/main/java/com/example/project1/PhoneBookData.kt
@@ -4,27 +4,26 @@ import android.os.Parcel
 import android.os.Parcelable
 
 class PhoneBookData(
-    val name: String,
-    val number: String
-) {}
-//) : Parcelable {
-//    constructor(source: Parcel) : this(
-//        source.readString(),
-//        source.readString()
-//    )
-//    override fun describeContents() = 0
-//    override fun writeToParcel(dest: Parcel?, flags: Int): Unit = with(dest) {
-//        this?.writeString(name)
-//        this?.writeString(number)
-//    }
-//
-//    companion object CREATOR : Parcelable.Creator<PhoneBookData> {
-//        override fun createFromParcel(parcel: Parcel): PhoneBookData {
-//            return PhoneBookData(parcel)
-//        }
-//
-//        override fun newArray(size: Int): Array<PhoneBookData?> {
-//            return arrayOfNulls(size)
-//        }
-//    }
-//}
+    val name: String?,
+    val number: String?
+) : Parcelable {
+    constructor(source: Parcel) : this(
+        source.readString(),
+        source.readString()
+    )
+    override fun describeContents() = 0
+    override fun writeToParcel(dest: Parcel?, flags: Int): Unit = with(dest) {
+        this?.writeString(name)
+        this?.writeString(number)
+    }
+
+    companion object CREATOR : Parcelable.Creator<PhoneBookData> {
+        override fun createFromParcel(parcel: Parcel): PhoneBookData {
+            return PhoneBookData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PhoneBookData?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
