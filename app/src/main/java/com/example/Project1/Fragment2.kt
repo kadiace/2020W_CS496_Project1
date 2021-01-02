63f327a9a1de263ad282fdcf7b71de721449a4e9
jane79 <jane79.bn@gamil.com>
2021-01-02 10:13:09 +0900

    Recyclerveiwing....

--- app/src/main/java/com/example/Project1/Fragment2.kt
+++ app/src/main/java/com/example/Project1/Fragment2.kt
@@ -3,8 +3,10 @@ package com.example.project1
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
@@ -17,25 +19,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.Project1.ImageData
 import com.example.Project1.GalleryAdapter
 import kotlinx.android.synthetic.main.fragment_2.view.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 class Fragment2 : Fragment() {
     //private val Gallery = 0
-    /*
-    private var imageList = arrayListOf<Image>(
-        Image(R.drawable.img_01),
-        Image(R.drawable.img_02),
-        Image(R.drawable.img_03),
-        Image(R.drawable.img_04),
-        Image(R.drawable.img_05),
-        Image(R.drawable.img_06),
-        Image(R.drawable.img_07),
-        Image(R.drawable.img_08)
-    )*/
-    private var imageList = emptyArray<Image>()
+    //private var imageList = emptyArray<Image>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+    override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_2)
+
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_2, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,93 +51,117 @@ class Fragment2 : Fragment() {
         val countImages = mutableListOf<Int>()
         val folderId = mutableListOf<Long>()
 
-        val condition:(ImageData, ImageData)->Boolean= { mdf1: ImageData, mdf2: ImageData -> mdf1.bucketID == mdf2.bucketID }
-        ImageDataset.forEach{
+        val condition: (ImageData, ImageData) -> Boolean =
+            { mdf1: ImageData, mdf2: ImageData -> mdf1.bucketID == mdf2.bucketID }
+        ImageDataset.forEach {
             if (listContainsContitionedItem(folderDataset, it, condition).not()) {
                 folderDataset.add(it)
                 countImages.add(1)
-                folderId.add(it.bucketId)
-            }
-            else{
-                countImages[folderId.indexOf(it.bucketId)] += 1
+                folderId.add(it.bucketID)
+            } else {
+                countImages[folderId.indexOf(it.bucketID)] += 1
             }
         }
 
-        val recyclerView: RecyclerView = view.gallery
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
-        recyclerView.adapter = GalleryAdapter(requireContext(), folderDataset, countImages)
-
-        /*
-        var gridview: GridView = view!!.findViewById(R.id.gridView)
-        val adapter = GalleryAdapter(context, imageList)
-        //Log.i("display","3")
-        gridview.adapter = adapter
-        //checkGalleryPermission()
-        displayGalleryImg()*/
+        val recyclerview: RecyclerView = view.gallery
+        recyclerview.layoutManager = GridLayoutManager(requireContext(), 3)
+        recyclerview.adapter = GalleryAdapter(requireContext(), ImageDataset, countImages)
     }
 
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_2, container,false)
-        return view
+    lateinit var currentPhotoPath: String
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = context!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
     }
 
-    fun getFileList(context: Context, type: Image): List<Image> {
-        val folderList = mutableListOf<Image>()
+    private fun getFileList(context: Context, type: MediaStoreFileType): List<ImageData> {
+        val imageList = mutableListOf<ImageData>()
         val projection = arrayOf(
-                MediaStore.Files.FileColumns._ID,
-                MediaStore.Files.FileColumns.DISPLAY_NAME,
-                MediaStore.Files.FileColumns.DATE_TAKEN,
-                MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME,
-                MediaStore.Files.FileColumns.BUCKET_ID
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.Files.FileColumns.DISPLAY_NAME,
+            MediaStore.Files.FileColumns.DATE_TAKEN,
+            MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME,
+            MediaStore.Files.FileColumns.BUCKET_ID
         )
         val sortOrder = "${MediaStore.Files.FileColumns.DATE_TAKEN} DESC"
         val cursor = context.contentResolver.query(
-                type.externalContentUri,
-                projection,
-                null,
-                null,
-                sortOrder
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            //projection,
+            null,
+            null,
+            null,
+            sortOrder
         )
+
         cursor?.use {
             val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID)
             val dateTakenColumn =
-                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_TAKEN)
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_TAKEN)
             val displayNameColumn =
-                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DISPLAY_NAME)
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DISPLAY_NAME)
             val bucketIDColumn =
-                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.BUCKET_ID)
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.BUCKET_ID)
             val bucketNameColumn =
-                    cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME)
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME)
             while (cursor.moveToNext()) {
                 val id = cursor.getLong(idColumn)
                 val dateTaken = Date(cursor.getLong(dateTakenColumn))
                 val displayName = cursor.getString(displayNameColumn)
                 val contentUri = Uri.withAppendedPath(
-                        type.externalContentUri,
-                        id.toString()
+                    type.externalContentUri,
+                    id.toString()
                 )
                 val bucketID = cursor.getLong(bucketIDColumn)
                 val bucketName = cursor.getString(bucketNameColumn)
                 Log.d(
-                        "test",
-                        "id: $id, display_name: $displayName, date_taken: $dateTaken, content_uri: $contentUri\n"
+                    "test",
+                    "id: $id, display_name: $displayName, date_taken: $dateTaken, content_uri: $contentUri\n"
                 )
-                val MDF = Image(id, dateTaken, displayName, contentUri, bucketID, bucketName)
-                folderList.add(MDF)
+                val MDF = ImageData(id, dateTaken, displayName, contentUri, bucketID, bucketName)
+                imageList.add(MDF)
             }
         }
-        return folderList
+        return imageList
+    }
+
+    private fun <E> listContainsContitionedItem(
+        list: MutableList<E>,
+        item: E,
+        condition: (E, E) -> Boolean
+    ): Boolean {
+        list.forEach {
+            when (condition(it, item)) {
+                true -> return true
+            }
+        }
+        return false
+    }
+
+    enum class MediaStoreFileType(
+        val externalContentUri: Uri,
+        val mimeType: String,
+        val pathByDCIM: String
+    ) {
+        IMAGE(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*", "/image"),
+        //AUDIO(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, "audio/*", "/audio"),
+        //VIDEO(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, "video/*", "/video");
     }
 
 
-    private fun checkGalleryPermission(){
+    private fun checkGalleryPermission() {
         //스토리지 읽기 퍼미션을 permission 변수에 담는다
         val permission = context?.let {
-        // context가 null이 아닐 경우만 시행
+            // context가 null이 아닐 경우만 시행
 //            ContextCompat.checkSelfPermission(
 //                it, Manifest.permission.READ_EXTERNAL_STORAGE)
         }
@@ -147,7 +177,9 @@ class Fragment2 : Fragment() {
 //            title.setText("YEP")
 //        }
     }
+}
 
+    /*
     private val OPEN_GALLERY = 1
     private fun displayGalleryImg(){
         val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
@@ -171,15 +203,15 @@ class Fragment2 : Fragment() {
                 if (allpath != null) {
                     for (string in allpath){
                         var item : Image? = null
-                        item?.sdcardPath = string
-                        imageList.add(string)
+                        //item?.sdcardPath = string
+                        //imageList.add(string)
                     }
                 }
             }
         }else{
             Log.d("ActivityResult", "something wrong")
         }
-    }
+    }*/
 
     ///////////LOCAL///////////
     private fun displayLocalImg() {
@@ -199,7 +231,7 @@ class Fragment2 : Fragment() {
         var n:Int = imageIDs.size
 
          */
-
+        /*
         // 사진들을 보여줄 GridView 뷰의 어댑터 객체를 정의하고 그것을 이 뷰의 어댑터로 설정합니다.
         Log.i("display","1")
         var gridview: GridView = view!!.findViewById(R.id.gridView)
@@ -207,7 +239,7 @@ class Fragment2 : Fragment() {
         val adapter = GalleryAdapter(context, imageList)
         Log.i("display","3")
         gridview.adapter = adapter
-        Log.i("display","4")
+        Log.i("display","4")*/
     }
 
 
